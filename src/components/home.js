e9de2de0a81fb0f757eb34baab68a297ce65eff6
vipulpathak113 <vipulpathak113@gmail.com>
2022-03-27 16:34:14 +0530

    questions added

--- src/components/home.js
+++ src/components/home.js
@@ -9,9 +9,9 @@ function home() {
           <div className="question-text">Question : </div>
           <div className="actual-question">What are Pseudo-classes?</div>
         </div>
+          <div className="answer-text">Answer :- </div>
         <div className="answer">
-          <div className="question-text">Answer : </div>
-          <div className="actual-question">
+          <div className="actual-answer">
             A pseudo-class is used to define a special state of an element.
             <pre>
               Syntax:
@@ -27,11 +27,11 @@ function home() {
       <div className="question-box">
         <div className="question">
           <div className="question-text">Question : </div>
-          <div className="actual-question">Whats is CDN</div>
+          <div className="actual-question">Whats is CDN?</div>
         </div>
+          <div className="answer-text">Answer :- </div>
         <div className="answer">
-          <div className="question-text">Answer : </div>
-          <div className="actual-question">
+          <div className="actual-answer">
             A content delivery network (CDN) is a group of geographically
             distributed servers that speed up the delivery of web content by
             bringing it closer to where users are.
@@ -43,11 +43,13 @@ function home() {
       <div className="question-box">
         <div className="question">
           <div className="question-text">Question : </div>
-          <div className="actual-question">Whats is a Promise</div>
-        </div>
-        <div className="answer">
-          <div className="question-text">Answer : </div>
           <div className="actual-question">
+            Whats is a Promise in javascript?
+          </div>
+        </div>
+        <div className="answer-text">Answer :- </div>
+        <div className="answer">
+          <div className="actual-answer">
             The Promise object represents the eventual completion (or failure)
             of an asynchronous operation and its resulting value.
             <pre className="example">Example:</pre>
@@ -68,17 +70,95 @@ function home() {
       <div className="question-box">
         <div className="question">
           <div className="question-text">Question : </div>
-          <div className="actual-question">Whats are methods of Promises</div>
+          <div className="actual-question">Whats are methods of Promises?</div>
         </div>
+        <div className="answer-text">Answer :- </div>
         <div className="answer">
-          <div className="question-text">Answer : </div>
-          <div className="actual-question"> <span style={{margin:"20px"}}>The methods of Promises are:</span> 
+          <div className="actual-answer">
+            {" "}
+            <span style={{ margin: "20px" }}>The methods of Promises are:</span>
             <ol>
-            <li>Promise.all() : It takes array of promises as an input and promise will reolve if all the input promises are resolved, if one fails it rejects the promise</li>
-            <li>Promise.allSettled() : It takes array of promises as an input and returns a promise after all the promises are fulfilled or rejected</li>
-            <li>Promise.any() : It takes array of promises as an input and returns a single promise as soon one of the promise is fulfilled and if promises are empty or all promise are rejected then it rejects the promise </li>
-            <li>Promise.race() : It takes array of promises as an input and returns a single promise as soon one of the promise is fulfilled or rejected</li>
-          </ol>
+              <li>
+                Promise.all() : It takes array of promises as an input and
+                promise will reolve if all the input promises are resolved, if
+                one fails it rejects the promise
+              </li>
+              <li>
+                Promise.allSettled() : It takes array of promises as an input
+                and returns a promise after all the promises are fulfilled or
+                rejected
+              </li>
+              <li>
+                Promise.any() : It takes array of promises as an input and
+                returns a single promise as soon one of the promise is fulfilled
+                and if promises are empty or all promise are rejected then it
+                rejects the promise{" "}
+              </li>
+              <li>
+                Promise.race() : It takes array of promises as an input and
+                returns a single promise as soon one of the promise is fulfilled
+                or rejected
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <div className="question-box">
+        <div className="question">
+          <div className="question-text">Question : </div>
+          <div className="actual-question">Whats is ECMAScript?</div>
+        </div>
+        <div className="answer-text">Answer :- </div>
+        <div className="answer">
+          <div className="actual-answer">
+            <span>
+            ECMAScript(European Computer Manufacturers Association Script) is
+            the standard that JavaScript programming language uses. ECMAScript
+            provides the specification on how JavaScript programming language
+            should work.
+            <pre>
+              JavaScript ES6 (also known as ECMAScript 2015 or ECMAScript 6) is
+              the newer version of JavaScript that was introduced in 2015.
+            </pre>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="question-box">
+        <div className="question">
+          <div className="question-text">Question : </div>
+          <div className="actual-question">Whats is a Babel?</div>
+        </div>
+        <div className="answer-text">Answer :- </div>
+        <div className="answer">
+          <div className="actual-answer">
+            <span>
+            It is a transpiler which is used to convert ES6+ code to backward comaptible version of js so that it can be used with current and old browser environment
+            <pre>
+              Basically it does 2 things:
+              <ol>
+                <li>It transform new syntax (Ex: let a ="xyz" is converted into var a ="xyz")</li>
+                <li>It uses polyfills to support new functions (like includes)</li>
+              </ol>
+            </pre>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="question-box">
+        <div className="question">
+          <div className="question-text">Question : </div>
+          <div className="actual-question">Whats is a Webpack?</div>
+        </div>
+        <div className="answer-text">Answer :- </div>
+        <div className="answer">
+          <div className="actual-answer">
+            <span>
+            Its a Module bundler which is use to bundle our code and assets (incuding static assets such as images) into a 'bundle' which we can then deploy. 
+            <pre>
+            Its main function is to create dependency graph so that all files can be injected in proper order
+            </pre>
+            </span>
           </div>
         </div>
       </div>
