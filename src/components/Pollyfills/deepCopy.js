7f089523391fe354eaa129fe430b63b40e245756
vipulpathak113 <vipulpathak113@gmail.com>
2023-09-26 13:16:49 +0530

    updating readme

--- src/components/Pollyfills/deepCopy.js
+++ src/components/Pollyfills/deepCopy.js
@@ -1,13 +1,18 @@
 const deepCopy = (obj) => {
-    if (typeof obj !== 'object' || obj === null) {
-        return obj;
-    }
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
 
-    const newObj = Array.isArray(obj) ? [] : {};
+  const newObj = Array.isArray(obj) ? [] : {};
 
-    for (const key in obj) {
-        newObj[key] = deepCopy(obj[key]);
-    }
+  for (const key in obj) {
+    newObj[key] = deepCopy(obj[key]);
+  }
 
-    return newObj;
-}
+  return newObj;
+};
+
+let val = { a: { b: 1 } };
+let result = deepCopy(val);
+result.a.b = 2;
+console.log(result, val); // val will not be changed
